f1e7217e40f927ee3b7dc79b04a06c8cdcc56b85
Tam Doan <tamdoan246@gmail.com>
2024-10-27 17:34:00 +0700

    Map_1

--- Assets/Tâm/Scripts/GroundSwitcher.cs
+++ Assets/Tâm/Scripts/GroundSwitcher.cs
@@ -1,6 +1,7 @@
-using Cinemachine;
+﻿using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
 
 public class GroundSwitcher : MonoBehaviour
@@ -13,6 +14,10 @@ public class GroundSwitcher : MonoBehaviour
 
     public SpriteRenderer playerOrderLayer;
 	public CinemachineVirtualCamera virtualCamera;
+
+	public readonly float middleGroundOrthographic = 7.5f;
+	public readonly float backGroundOrthographic = 6;
+
 	Camera cam;
     // Start is called before the first frame update
     void Start()
@@ -25,21 +30,65 @@ public class GroundSwitcher : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.W))
         {
-            playerOrderLayer.sortingOrder = -3;
-            virtualCamera.m_Lens.OrthographicSize = 6f;
-			int middlegroundLayer = LayerMask.NameToLayer("MiddleGround");
-			cam.cullingMask = ~(1 << middlegroundLayer);
-            transform.localScale = playerBackGroundScale;
-            transform.position = new Vector3(transform.position.x, backGroundY, -10);
+			StartCoroutine(MoveYOverTime(backGroundY, false));
         }
         else if (Input.GetKeyDown(KeyCode.S))
         {
-            playerOrderLayer.sortingOrder = 0;
-			virtualCamera.m_Lens.OrthographicSize = 7.5f;
-			cam.cullingMask = -1;
-			transform.localScale = playerMiddleGroundScale;
-			transform.position = new Vector3(transform.position.x, middleGroundY, -10);
+			playerOrderLayer.sortingOrder = 0;
+			StartCoroutine(MoveYOverTime(middleGroundY, true));
 		}
 
+	}
+
+	private IEnumerator MoveYOverTime(float tagertY, bool isScaleUp)
+	{
+		float startY = transform.position.y; // Lấy vị trí Y ban đầu
+		float elapsedTime = 0f; // Thời gian đã trôi qua
+		Vector3 startScale = transform.localScale;
+		Vector3 endScale = isScaleUp ? playerMiddleGroundScale : playerBackGroundScale;
+		float startOrthographic = virtualCamera.m_Lens.OrthographicSize;
+		float endOrthographic = isScaleUp ? middleGroundOrthographic : backGroundOrthographic;
+		float timer = 1f;
+
+		endScale.x *= Mathf.Sign(transform.localScale.x); //Lấy dấu hướng X
+
+		while (elapsedTime < timer)
+		{
+			// Tăng thời gian đã trôi qua
+			elapsedTime += Time.deltaTime;
+			if(isScaleUp && elapsedTime >= timer / 2)
+			{
+				cam.cullingMask = -1;
+			}
+			// Tính toán giá trị Y hiện tại sử dụng Lerp
+			float newY = Mathf.Lerp(startY, tagertY, elapsedTime / timer);
+			Vector3 newScale = Vector3.Lerp(startScale, endScale, elapsedTime / timer);
+			float newOrthographic = Mathf.Lerp(startOrthographic, endOrthographic, elapsedTime / timer);
+
+			// Cập nhật vị trí của đối tượng
+			transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+			transform.localScale = newScale;
+			virtualCamera.m_Lens.OrthographicSize = newOrthographic;
+
+			// Chờ một frame trước khi tiếp tục
+			yield return null;
+		}
+
+		// Đảm bảo đối tượng đạt đến vị trí Y mục tiêu
+		transform.position = new Vector3(transform.position.x, tagertY, transform.position.z);
+		transform.localScale = endScale;
+		virtualCamera.m_Lens.OrthographicSize = endOrthographic;
+
+
+		if (isScaleUp)
+		{
+			playerOrderLayer.sortingOrder = 0;
+		}
+		else
+		{
+			playerOrderLayer.sortingOrder = -3;
+			int middlegroundLayer = LayerMask.NameToLayer("MiddleGround");
+			cam.cullingMask = ~(1 << middlegroundLayer);
+		}
 	}
 }
